56ac91e4ad92c16927255b326b920e6ff77c80d3
Indrek Luuk <indrek.luuk@gmail.com>
2021-08-31 00:20:24 +0300

    Fixed grayscale

--- src/lib/LiveOV7670Library/BufferedCameraOV7670_QQVGA_10hz_Grayscale.h
+++ src/lib/LiveOV7670Library/BufferedCameraOV7670_QQVGA_10hz_Grayscale.h
@@ -15,29 +15,29 @@ class BufferedCameraOV7670_QQVGA_10hz_Grayscale : public BufferedCameraOV7670<ui
 public:
   BufferedCameraOV7670_QQVGA_10hz_Grayscale() : BufferedCameraOV7670(Resolution::RESOLUTION_QQVGA_160x120, CameraOV7670::PIXEL_YUV422, 0) {};
 
+  void ignoreVerticalPadding() override;
   void readLine() override;
 
 };
 
 
 
+void BufferedCameraOV7670_QQVGA_10hz_Grayscale::ignoreVerticalPadding() {
+  for (uint8_t i = 0; i < verticalPadding; i++) {
+    readLine();
+  }
+}
+
 
 void BufferedCameraOV7670_QQVGA_10hz_Grayscale::readLine() {
   pixelBuffer.writeBufferPadding = 0;
-  waitForPixelClockLow();
 
-  asm volatile("nop");
-  asm volatile("nop");
-  asm volatile("nop");
+  waitForPixelClockLow();
+  waitForPixelClockHigh();
 
   for (uint8_t i = 0; i<160; i++) {
+    waitForPixelClockLow();
     readPixelByte(pixelBuffer.readBuffer[i]);
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
-    asm volatile("nop");
   }
 }
 
